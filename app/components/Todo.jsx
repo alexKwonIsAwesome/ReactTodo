b8bd3c4314e5183e8fdb78ec7997ed6f0020bf36
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-26 03:34:02 +0900

    update Todo: data rendering format changes

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -2,10 +2,10 @@ var React = require('react');
 
 var Todo = React.createClass({
   render: function () {
-    var {text} = this.props;
+    var {text, id} = this.props;
     return (
       <div>
-        {text}
+      {id}. {text}
       </div>
     )
   }
